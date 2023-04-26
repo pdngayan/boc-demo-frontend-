bd4c463f13251a07f8c4e8adfa9154db987df7dd
Gayan <gayan.dissanayake@ncinga.net>
2023-04-26 10:52:00 +0530

    lable change

--- src/ClientList.js
+++ src/ClientList.js
@@ -51,7 +51,7 @@ class ClientList extends Component {
                 <AppNavbar/>
                 <Container fluid>
                     <div className="float-right">
-                        <Button color="success" tag={Link} to="/clients/new">Add Client</Button>
+                        <Button color="success" tag={Link} to="/clients/new">Add Client Test</Button>
                     </div>
                     <h3>Clients</h3>
                     <Table className="mt-4">
